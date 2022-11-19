9e7cdadd1bf1107febc23092e61ca9b522a07152
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-19 16:59:51 -0700

    Added to comments

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -4,6 +4,8 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="post-title"]').value;
     const post_content = document.querySelector('textarea[name="post-content"]').value.trim();
 
+
+
     const response = await fetch(`/api/posts`, {
         method: 'POST',
         body: JSON.stringify({
@@ -21,5 +23,8 @@ async function newFormHandler(event) {
         alert(response.statusText);
     }
 }
+
+
+
 
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
