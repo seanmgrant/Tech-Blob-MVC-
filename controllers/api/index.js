5140b5ba9cd5f277097b4060ec24fdfc691a9aab
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-19 18:35:24 -0700

    added node to gitigonor

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes.js');
+const postRoutes = require('./post.js');
 const commentRoutes = require('./comment-routes.js');
 
 router.use('/users', userRoutes);
