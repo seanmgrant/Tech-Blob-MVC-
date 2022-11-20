eaaa2e29d8cb593dba25fa6b9246ea51f919d835
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-20 15:00:10 -0700

    Fixed format date back

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,5 @@
 module.exports = {
-    formatDate: date => {
+    format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
     }
 }
