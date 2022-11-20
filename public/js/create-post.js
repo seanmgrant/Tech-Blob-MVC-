fb482453420768f2bc984c42356cf682bf4536a4
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-19 17:03:34 -0700

    Added delete

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,10 +1,5 @@
 async function createPostHandler(event) {
     event.preventDefault();
-
     document.location.replace('/dashboard/new')
 }
-
-
-
-
 document.querySelector('#create-new-post').addEventListener('click', createPostHandler);
